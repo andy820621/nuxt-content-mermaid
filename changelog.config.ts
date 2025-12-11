afb284ff1a5b11626a8f868cbfb728a75f0d07ea
BarZ <andy820621@gmail.com>
2025-12-11 16:02:03 +0800

    chore: add changelog configuration file

---
+++ changelog.config.ts
@@ -0,0 +1,13 @@
+export default {
+  exclude: {
+    scopes: ['release'],
+  },
+  // types: {
+  //   feat: { title: '🚀 Enhancements' },
+  //   fix: { title: '🩹 Fixes' },
+  //   docs: { title: '📖 Documentation' },
+  //   perf: { title: '⚡ Performance' },
+  //   refactor: { title: '💅 Refactors' },
+  //   test: { title: '✅ Tests' },
+  // },
+}
