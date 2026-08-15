60af3704685c4e4e7e127da57391309a52d4ba5b
BarZ <andy820621@gmail.com>
2026-08-15 17:54:06 +0800

    chore(website): expose brand assets

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,8 +1,20 @@
+import { fileURLToPath } from 'node:url'
+
+const brandAssetsDir = fileURLToPath(new URL('../src/assets', import.meta.url))
+
 export default defineNuxtConfig({
   modules: [
     '@nuxt/content',
     '@barzhsieh/nuxt-content-mermaid',
   ],
   css: ['~/assets/css/main.css'],
+  nitro: {
+    publicAssets: [
+      {
+        baseURL: '/assets',
+        dir: brandAssetsDir,
+      },
+    ],
+  },
   compatibilityDate: '2025-11-24',
 })
