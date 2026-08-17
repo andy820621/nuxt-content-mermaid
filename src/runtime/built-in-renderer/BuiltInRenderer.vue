12b1723ad2b2ce337a7cb930fcabd63ed312f749
BarZ <andy820621@gmail.com>
2026-08-17 21:36:44 +0800

    fix(renderer): center Mermaid SVGs

--- src/runtime/built-in-renderer/BuiltInRenderer.vue
+++ src/runtime/built-in-renderer/BuiltInRenderer.vue
@@ -795,6 +795,7 @@ const { cursorVariables } = useMermaidCursors(iconSize, expandEnabled)
   height: 100%;
 }
 .mermaid-wrapper > div > :deep(svg) {
+  display: block;
   margin: 0 auto;
 }
 .mermaid-wrapper :deep(pre),
